fc6f81c1b53e8c00af894e4188d9237a076d3da9
Brian McBrayer <brian.mcbrayer@hpe.com>
2021-12-03 13:35:35 -0500

    Trying again

--- sources/index.ts
+++ sources/index.ts
@@ -1,21 +1,41 @@
-import {Plugin} from '@yarnpkg/core';
+import {Descriptor, Locator, Plugin, Project, Report, ResolveOptions, Resolver} from '@yarnpkg/core';
 import {URL} from 'url';
 
 // change this when yarn is released
 // import {WrapNetworkRequestInfo} from "@yarnpkg/core";
 
-async function wrapNetworkRequest(executor: () => Promise<any>, {target}: { target: string | URL }) {
-  const url = typeof target === `string` ? new URL(target) : target;
+class YarnPluginPackageAudit {
+  private report: Report;
 
-  console.log(`YNX000: Making network request to ${url.href}`)
+  async reduceDependency(
+    _dependency: Descriptor,
+    _project: Project,
+    _locator: Locator,
+    _initialDependency: Descriptor,
+    extra: { resolver: Resolver; resolveOptions: ResolveOptions }
+  ) {
+    this.report = extra.resolveOptions.report;
+  }
 
-  return await executor();
-};
+  async wrapNetworkRequest(
+    executor: () => Promise<any>,
+    { target }: { target: string | URL }
+  ) {
+    const url = typeof target === `string` ? new URL(target) : target;
+
+    this.report.reportInfo(null, `YNX000: fetching from ${url.href}`);
+
+    return await executor();
+  }
+}
+
+const audit = new YarnPluginPackageAudit();
 
 const plugin: Plugin = {
   hooks: {
-    wrapNetworkRequest
-  }
+    wrapNetworkRequest: audit.wrapNetworkRequest,
+    reduceDependency: audit.reduceDependency
+  },
 };
 
 export default plugin;
