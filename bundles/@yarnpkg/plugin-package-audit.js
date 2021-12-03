61b407f4b2ffae4aa68f37dc28d3581d1f8e3faa
Brian McBrayer <brian.mcbrayer@hpe.com>
2021-12-03 13:38:40 -0500

    2

--- bundles/@yarnpkg/plugin-package-audit.js
+++ bundles/@yarnpkg/plugin-package-audit.js
@@ -3,7 +3,84 @@
 module.exports = {
 name: "@yarnpkg/plugin-package-audit",
 factory: function (require) {
-var plugin=(()=>{var y=Object.create,s=Object.defineProperty;var w=Object.getOwnPropertyDescriptor;var f=Object.getOwnPropertyNames;var v=Object.getPrototypeOf,D=Object.prototype.hasOwnProperty;var g=e=>s(e,"__esModule",{value:!0});var m=e=>{if(typeof require!="undefined")return require(e);throw new Error('Dynamic require of "'+e+'" is not supported')};var h=(e,r)=>{for(var o in r)s(e,o,{get:r[o],enumerable:!0})},k=(e,r,o)=>{if(r&&typeof r=="object"||typeof r=="function")for(let t of f(r))!D.call(e,t)&&t!=="default"&&s(e,t,{get:()=>r[t],enumerable:!(o=w(r,t))||o.enumerable});return e},P=e=>k(g(s(e!=null?y(v(e)):{},"default",e&&e.__esModule&&"default"in e?{get:()=>e.default,enumerable:!0}:{value:e,enumerable:!0})),e);var p=(e,r,o)=>{if(!r.has(e))throw TypeError("Cannot "+o)};var c=(e,r,o)=>(p(e,r,"read from private field"),o?o.call(e):r.get(e)),i=(e,r,o)=>{if(r.has(e))throw TypeError("Cannot add the same private member more than once");r instanceof WeakSet?r.add(e):r.set(e,o)},l=(e,r,o,t)=>(p(e,r,"write to private field"),t?t.call(e,o):r.set(e,o),o);var O={};h(O,{default:()=>N});var u=P(m("url")),n,a=class{constructor(){i(this,n,void 0)}async reduceDependency(r,o,t,_,R){l(this,n,R.resolveOptions.report)}async wrapNetworkRequest(r,{target:o}){let t=typeof o=="string"?new u.URL(o):o;return c(this,n).reportInfo(null,`YNX000: fetching from ${t.href}`),await r()}};n=new WeakMap;var d=new a,L={hooks:{wrapNetworkRequest:d.wrapNetworkRequest,reduceDependency:d.reduceDependency}},N=L;return O;})();
+var plugin = (() => {
+  var __create = Object.create;
+  var __defProp = Object.defineProperty;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __getProtoOf = Object.getPrototypeOf;
+  var __hasOwnProp = Object.prototype.hasOwnProperty;
+  var __markAsModule = (target) => __defProp(target, "__esModule", {value: true});
+  var __require = (x) => {
+    if (typeof require !== "undefined")
+      return require(x);
+    throw new Error('Dynamic require of "' + x + '" is not supported');
+  };
+  var __export = (target, all) => {
+    for (var name in all)
+      __defProp(target, name, {get: all[name], enumerable: true});
+  };
+  var __reExport = (target, module, desc) => {
+    if (module && typeof module === "object" || typeof module === "function") {
+      for (let key of __getOwnPropNames(module))
+        if (!__hasOwnProp.call(target, key) && key !== "default")
+          __defProp(target, key, {get: () => module[key], enumerable: !(desc = __getOwnPropDesc(module, key)) || desc.enumerable});
+    }
+    return target;
+  };
+  var __toModule = (module) => {
+    return __reExport(__markAsModule(__defProp(module != null ? __create(__getProtoOf(module)) : {}, "default", module && module.__esModule && "default" in module ? {get: () => module.default, enumerable: true} : {value: module, enumerable: true})), module);
+  };
+  var __accessCheck = (obj, member, msg) => {
+    if (!member.has(obj))
+      throw TypeError("Cannot " + msg);
+  };
+  var __privateGet = (obj, member, getter) => {
+    __accessCheck(obj, member, "read from private field");
+    return getter ? getter.call(obj) : member.get(obj);
+  };
+  var __privateAdd = (obj, member, value) => {
+    if (member.has(obj))
+      throw TypeError("Cannot add the same private member more than once");
+    member instanceof WeakSet ? member.add(obj) : member.set(obj, value);
+  };
+  var __privateSet = (obj, member, value, setter) => {
+    __accessCheck(obj, member, "write to private field");
+    setter ? setter.call(obj, value) : member.set(obj, value);
+    return value;
+  };
+
+  // pnp:/mnt/c/code/package-audit-plugin/sources/index.ts
+  var sources_exports = {};
+  __export(sources_exports, {
+    default: () => sources_default
+  });
+  var import_url = __toModule(__require("url"));
+  var _report;
+  var YarnPluginPackageAudit = class {
+    constructor() {
+      __privateAdd(this, _report, void 0);
+    }
+    async reduceDependency(_dependency, _project, _locator, _initialDependency, extra) {
+      __privateSet(this, _report, extra.resolveOptions.report);
+    }
+    async wrapNetworkRequest(executor, {target}) {
+      const url = typeof target === `string` ? new import_url.URL(target) : target;
+      __privateGet(this, _report).reportInfo(null, `YNX000: fetching from ${url.href}`);
+      return await executor();
+    }
+  };
+  _report = new WeakMap();
+  var audit = new YarnPluginPackageAudit();
+  var plugin = {
+    hooks: {
+      wrapNetworkRequest: audit.wrapNetworkRequest,
+      reduceDependency: audit.reduceDependency
+    }
+  };
+  var sources_default = plugin;
+  return sources_exports;
+})();
 return plugin;
 }
 };
